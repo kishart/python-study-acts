e1540050aa11d9b870b1ca60feb9c8d59dc5844f
kishart <amoralyse21@gmail.com>
2024-03-09 12:03:30 +0800

    another act

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 
 #print ("Hello World!")
 
-character_job="pirate"
-character_ride="ship"
-character_souvenir="gold"
-character_pet="parrot"
+#coding exercise
+#character_job="pirate"
+#character_ride="ship"
+#character_souvenir="gold"
+#character_pet="parrot"
 
-print("There was once a " + character_job + " who loved adventure.")
-print("The " + character_job + " would take her " + character_ride + " to unknown places.")
-print("She brings home a lot of " + character_souvenir)
-print("Then she goes home to her pet " + character_pet + ", Chuckles.")
+#print("There was once a " + character_job + " who loved adventure.")
+#print("The " + character_job + " would take her " + character_ride + " to unknown places.")
+#print("She brings home a lot of " + character_souvenir)
+#print("Then she goes home to her pet " + character_pet + ", Chuckles.")
+
+
+#print("Roses are red, violets are blue")
+
+#if 10>7:
+   # print("ten is greater than seven!")
+#if 16<42:
+   # print("sixteen is less than forty-two!")
+
+#print("a long time ago, in a galaxy far, far away...")
