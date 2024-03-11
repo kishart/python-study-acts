8b9822dfa35b15fac67465fe9d6cc5b8a5c711cc
kishart <amoralyse21@gmail.com>
2024-03-12 00:16:42 +0800

    uwu

--- main.py
+++ main.py
@@ -131,8 +131,18 @@
 
 
 #for loops
-furniture =["table", "chair", "cabinet","desk", "couch"]
-for x in furniture:
-      if x == "cabinet":
-         continue
-      print(x)
+#furniture =["table", "chair", "cabinet","desk", "couch"]
+#for x in furniture:
+ #     if x == "cabinet":
+ #        continue
+ #     print(x)
+
+
+#while loops
+i=1
+while i<15:
+   print(i)
+   i+=1
+   
+else:
+   print("i is no longer less than 15")
