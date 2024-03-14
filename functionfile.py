d219636de85a7f07c16e93e41a3ba894875eb398
peach <108270895+kishart@users.noreply.github.com>
2024-03-14 14:52:50 +0800

    Create functionfile.py

---
+++ functionfile.py
@@ -0,0 +1,14 @@
+def general_greeting():
+    print("Hello everyone!")
+    
+def personal_greeting(name):
+    print("Hi, " + name + "!")
+
+def your_province(province):
+    print("Oh, I heard " + province + " is a nice place") 
+    
+def get_Sum(x,y):
+    print(x+y)
+    
+def get_Diff(x,y):
+    print(x-y)
